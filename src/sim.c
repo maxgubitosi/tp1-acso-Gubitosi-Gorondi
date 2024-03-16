52b38e66b48aa6bdd8273eb31568350972a89914
maxgubitosi <mgubitosi@udesa.edu.ar>
2024-03-16 19:33:25 -0300

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -37,11 +37,87 @@ void fetch() {
     19. MOVZ
 */
 
+uint32_t rd;
+uint32_t rn;
+uint32_t rm;
+
 // ------------------------- DECODE -------------------------
+void decode()
+{
+    fetch();
+
+    // 1. identify opcode format:
+    
+    // case B: B or BR
+    uint32_t opcode = curr_instr >> 26;
+    
+    opcode = curr_instr >> 21;
+    if (curr_instr==0b10101011001) { // quizas es 0b10101011000 segun tp
+        // ADDS (extended register)
+        rd = curr_instr & 0x0000001f; 
+        rn = curr_instr & 0x000003e0;
+        rn = rn >> 5;
+        // uint32_t shamt = curr_instr & 0x0000fc00;
+        // shamt = shamt >> 10;
+        rm = curr_instr & 0x001f0000;
+        rm = rm >> 16;
+        execute_ADDS();
+    }
+
+    // // B (pag 550) 
+    // if (opcode == 0b000101) {
+    //     // execute_B();
+    // }
+    // // BR (pag 562)
+    // else if (opcode == 0b110101) {
+    //     // execute_BR();
+    // }
+
+    // // case CB:  
+    // opcode = curr_instr >> 24;
+
+    // // B.COND (pag 549)    
+    // if (opcode == 0b01010100) {
+    //     // execute_B_COND();
+    // } // chequear sub casos (BEQ, BNE, BGT, BLT, BGE, BLE) mirar desp del opcode
+
+    // // case I:
+    // opcode = curr_instr >> 22;
+
+    // // CMP
+    // if (opcode == 0b1101000100) {
+    //     // execute_CMP();
+    // }
 
 
+    // LSL
 
+    // LSR
 
+    // case I:
+    // case D:
+    // case R:
+    // case IW:
+}
+
+void execute_ADDS() {
+    int64_t op1 = CURRENT_STATE.REGS[rn];
+    int64_t op2 = CURRENT_STATE.REGS[rm];
+    // exec_ALU(op1, op2, &N, &Z, &V, &C, &result, instr_name);
+    int64_t result = op1 + op2;
+    if (result < 0){
+        NEXT_STATE.FLAG_N = 1;
+    }else{
+        NEXT_STATE.FLAG_N = 0;
+    }
+    if (result == 0){
+        NEXT_STATE.FLAG_Z = 1;
+    }else{
+        NEXT_STATE.FLAG_Z = 0;
+    }
+
+    NEXT_STATE.REGS[rd] = result; 
+}
 
 void process_instruction()
 {
@@ -53,6 +129,6 @@ void process_instruction()
      *             y otra para execute()
      * 
      * */
-
+    decode();
 
 }
