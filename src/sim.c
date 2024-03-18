c93fd4696fcd153fd0f57fbd911e4ac84568ae52
maxgubitosi <mgubitosi@udesa.edu.ar>
2024-03-18 19:50:24 -0300

    Update sim.c

--- src/sim.c
+++ src/sim.c
@@ -5,9 +5,14 @@
 
 #define DEBUG 1        // 1 para debuggear
 
-#define ADD 1
-#define ADDI 2
+#define ADDS 1
+#define ADDSI 2
 #define HLT 3
+#define SUBS 4
+# define SUBSI 5
+
+
+// probar enummerate
 
 
 uint32_t curr_instr;  // current instruction
@@ -73,21 +78,31 @@ void execute_ADDS_ext() {
     if (DEBUG == 1) {printf("execute_ADDS\n");}
     int64_t op1 = CURRENT_STATE.REGS[rn];
     int64_t op2 = CURRENT_STATE.REGS[rm];
-    // exec_ALU(op1, op2, &N, &Z, &V, &C, &result, instr_name);
     result = op1 + op2;
-
     NEXT_STATE.REGS[rd] = result; 
 }
 
+void execute_SUBS_ext() {
+    if (DEBUG == 1) {printf("execute_SUBS\n");}
+    int64_t op1 = CURRENT_STATE.REGS[rn];
+    int64_t op2 = CURRENT_STATE.REGS[rm];
+    result = op1 - op2;
+    NEXT_STATE.REGS[rd] = result; 
+}
 void execute_ADDS_imm() {
     if (DEBUG == 1) {printf("execute_ADDS\n");}
     int64_t op1 = CURRENT_STATE.REGS[rn];
     int64_t op2 = imm;
-    // exec_ALU(op1, op2, &N, &Z, &V, &C, &result, instr_name);
     result = op1 + op2;
-
     NEXT_STATE.REGS[rd] = result; 
+}
 
+void execute_SUBS_imm() {
+    if (DEBUG == 1) {printf("execute_SUBSI\n");}
+    int64_t op1 = CURRENT_STATE.REGS[rn];
+    int64_t op2 = imm;
+    result = op1 - op2;
+    NEXT_STATE.REGS[rd] = result;
 }
 
 void execute_HLT() {
@@ -95,35 +110,47 @@ void execute_HLT() {
     RUN_BIT = FALSE;
 }
 
+void set_flags() {
+    if (result < 0){
+        NEXT_STATE.FLAG_N = 1;
+    }else{
+        NEXT_STATE.FLAG_N = 0;
+    }
+    if (result == 0){
+        NEXT_STATE.FLAG_Z = 1;
+    }else{
+        NEXT_STATE.FLAG_Z = 0;
+    }
+}
+
 void execute() {
     if (DEBUG == 1) {printf("execute\n");}
         switch (instr_name) {
-            case ADDI:
+            case ADDSI:
                 execute_ADDS_imm();
                 break;
-            case ADD:
+            case ADDS:
                 execute_ADDS_ext();
                 break;
             case HLT:
                 execute_HLT();
                 break;
+            case SUBS:
+                execute_SUBS_ext();
+                break;
+            case SUBSI:
+                execute_SUBS_imm();
+                break;
+
             default:
                 break;
             
-        if (result < 0){
-            NEXT_STATE.FLAG_N = 1;
-        }else{
-            NEXT_STATE.FLAG_N = 0;
-        }
-        if (result == 0){
-            NEXT_STATE.FLAG_Z = 1;
-        }else{
-            NEXT_STATE.FLAG_Z = 0;
-        }
+        set_flags();
 
     }
     NEXT_STATE.PC += 4;
 }
+
 // ------------------------- DECODE -------------------------
 void decode()
 {
@@ -148,18 +175,29 @@ void decode()
     if (DEBUG == 1) {printf("opcode: %d\n", opcode);}
     if (opcode == 0b10101011000) { // quizas es 0b10101011000 segun tp
         // ADDS (extended register)
+        rd = curr_instr & 0x0000001f;   // mask bits 0-4
+        rn = curr_instr & 0x000003e0;   // mask bits 5-9
+        rn = rn >> 5;
+        rm = curr_instr & 0x001f0000;  // mask bits 16-20
+        rm = rm >> 16;
+        if (DEBUG == 1) {printf("if\n");}
+        instr_name = ADDS;
+    }
+
+    // SUBS EXTENDED
+    if (DEBUG == 1) {printf("opcode: %d\n", opcode);}
+    if (opcode == 0b11101011001) { 
+        // ADDS (extended register)
         rd = curr_instr & 0x0000001f; 
         rn = curr_instr & 0x000003e0;
         rn = rn >> 5;
-        // uint32_t shamt = curr_instr & 0x0000fc00;
-        // shamt = shamt >> 10;
         rm = curr_instr & 0x001f0000;
         rm = rm >> 16;
         if (DEBUG == 1) {printf("if\n");}
-        instr_name = ADD;
+        instr_name = SUBS;
     }
 
-    opcode = opcode >> 1;
+    opcode = opcode >> 1; // shiftea total 22
     // ADDS IMMEDIATE
     if (DEBUG == 1) {printf("opcode: %d\n", opcode);}
     if (opcode == 0b1011000100 || opcode == 0b1011000101) { 
@@ -178,8 +216,28 @@ void decode()
         } else if (shift == 0b0000000001) {
             // pass
         } 
-        instr_name = ADDI;
+        instr_name = ADDSI;
     }
+
+    // SUBS IMMEDIATE
+    if (DEBUG == 1) {printf("opcode: %d\n", opcode);}
+    if (opcode == 0b1111000100 || opcode == 0b1111000101) { 
+        if (DEBUG == 1) {printf("if_SUBS_IMMEDIATE\n");}
+        rd = curr_instr & 0x0000000f;  // mask last 4 bits
+        rn = curr_instr & 0x000001f0;  // mask bits 5-9 bits
+        rn = rn >> 5; 
+        imm = curr_instr & 0x003ffc00; // mask bits 16-21 bits (12)
+        uint32_t shift = opcode & 0b0000000001;
+        if (shift == 0b0000000000) {
+            imm = imm >> 10;
+        } else if (shift == 0b0000000001) {
+            // pass
+        } 
+        instr_name = SUBSI;
+    }
+
+    // SUBS IMMEDIATE
+
 
     // // B (pag 550) 
     // if (opcode == 0b000101) {
